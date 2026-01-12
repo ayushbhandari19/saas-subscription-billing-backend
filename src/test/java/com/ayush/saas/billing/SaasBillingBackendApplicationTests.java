b162971e77c8e6bc90c1fdb5c97fcef4b8e40e04
nextlife <ayush.bhandari2001@gmail.com>
2026-01-12 20:08:49 +0530

    Initial Spring Boot SaaS backend

---
+++ src/test/java/com/ayush/saas/billing/SaasBillingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.saas.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasBillingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
